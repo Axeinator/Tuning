f911c0035b87ced675d50367265531e1ac35fc4d
Axeinator <33792363+Axeinator@users.noreply.github.com>
2021-04-26 13:04:23 -0500

    Added constraints to octave

--- src/OctaveSelect.svelte
+++ src/OctaveSelect.svelte
@@ -1,11 +1,16 @@
 <script>
     import { octave } from './stores.js';
+    import { get } from 'svelte/store'
 
     function increaseOctave() {
-        octave.update(n => n + 1);
+        if (get(octave) < 8) {
+            octave.update(n => n + 1);
+        }
     }
     function decreaseOctave() {
-        octave.update(n => n - 1);
+        if (get(octave) > 0) {
+            octave.update(n => n - 1);
+        }
     }
 </script>
 <button on:click="{increaseOctave}">
